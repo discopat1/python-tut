e341570a299a0d8af5079a7148b034ea1101907b
Patrick Neff <pbneff@yahoo.com>
2019-08-10 16:39:58 -0600

    loops

---
+++ loops.py
@@ -0,0 +1,18 @@
+string_variable = "HELLO WORLD"
+for character in string_variable:
+    print character
+
+list_variable = ["hello", "world"]
+for item in list_variable:
+    print item
+
+a = range(10)
+for i in a:
+    print i
+    print "hello"
+
+count = 0
+while (count<5):
+    print count
+    count = count+1
+print "goodbye"
--- statement.py
+++ statement.py
@@ -5,3 +5,20 @@ elif num==0:
     print "number is zero"
 else:
     print "number is negative"
+
+
+for var in "string":
+    if var=="i":
+        break
+        print(var)
+    else:
+        print(var)
+print("hello")
+
+
+for var in "string":
+    if var=="i":
+        continue
+        print(var)
+    else:
+        print(var)
