858be5e3870b245dfa7f75adec9ede1c3ae2d73f
Patrick Neff <pbneff@yahoo.com>
2019-08-25 12:43:42 -0600

    strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+a = "hello world"
+print a.capitalize()
+
+print a.count('l')
+
+print a.find('e')
+
+b = 'hello1233hwllo'
+print b.isalnum()
+print b.isalpha()
+print (len(b))
+print min(b)
+
+c = '1234'
+print c.isdigit()
+
+d = 'HELlo worLd'
+print d.lower()
+print d.upper()
+print d.swapcase()
+print max(d)
+print min(d)
