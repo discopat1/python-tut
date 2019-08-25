5b3d17d647176362bf825423092ecdbaa0a7adc5
Patrick Neff <pbneff@yahoo.com>
2019-08-25 12:25:43 -0600

    numbers

---
+++ numbers.py
@@ -0,0 +1,26 @@
+import math
+
+a = -12
+print abs(a)
+
+b = 12.23
+print (math.ceil(b))
+
+c = 13.45
+print (math.floor(c))
+
+print (math.exp(2))
+
+d = (12,45, 2)
+print (d)
+
+print max(d)
+print min(d)
+
+print (pow(2,3))
+
+print math.sqrt(100)
+
+print math.modf(12.9)
+
+print math.log(10,10)
